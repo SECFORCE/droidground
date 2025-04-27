b82ca212857a2b917712b259f1933faf780f7103
thelicato <thelicato@users.noreply.github.com>
2025-04-27 20:25:14 +0200

    feat: add basic UI

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,3 +1,226 @@
+import { DataMetadata, StreamingPhase, WSCallback, WSMessageType } from "@shared/types";
+import { ScrcpyVideoCodecId, ScrcpyVideoStreamMetadata } from "@yume-chan/scrcpy"
+import type { ScrcpyMediaStreamPacket } from "@yume-chan/scrcpy";
+import type { VideoFrameRenderer } from "@yume-chan/scrcpy-decoder-webcodecs";
+import { TinyH264Decoder } from "@yume-chan/scrcpy-decoder-tinyh264";
+import {
+  WebGLVideoFrameRenderer,
+  BitmapVideoFrameRenderer,
+  WebCodecsVideoDecoder
+} from "@yume-chan/scrcpy-decoder-webcodecs";
+import React, { useEffect, useRef, useState } from "react";
+import Lottie from "lottie-react";
+import bootData from '@client/assets/boot.json'
+import { useWebSocket, WebSocketProvider } from "./context/WebSocket";
+
+
+interface EnhancedStreamMetadata extends ScrcpyVideoStreamMetadata {
+  hardwareType: "hardware" | "software" | "hybrid",
+  encoder: string
+}
+
+enum Renderer {
+  TinyH264,
+  WebCodecs
+};
+
+const TinyH264Renderer: React.FC = () => {
+  const { sendMessage, subscribe, unsubscribe, streamingPhase } = useWebSocket();
+    // Store latest state in a ref to avoid stale closures
+  const controllerRef = useRef<ReadableStreamDefaultController<ScrcpyMediaStreamPacket> | null>(null)
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const configListener: WSCallback = async (_metadata, binaryData) => {
+      if (!binaryData) return;
+      controllerRef.current?.enqueue({ type: "configuration", data: binaryData });
+      sendMessage(WSMessageType.CONFIGURATION_ACK)
+    }
+
+    const dataListener: WSCallback = async (m, binaryData) => {
+      const metadata = m as DataMetadata;
+      if (!binaryData) return;
+      controllerRef.current?.enqueue({
+        type: "data",
+        keyframe: metadata.keyframe,
+        pts: metadata.pts ? BigInt(metadata.pts) : undefined,
+        data: binaryData,
+      });
+    }
+
+    const start = () => {
+      try {
+        const stream = new ReadableStream<ScrcpyMediaStreamPacket>({
+          start(controller) {
+            controllerRef.current = controller;
+          }
+        });
+
+        subscribe(WSMessageType.CONFIGURATION, configListener)
+        subscribe(WSMessageType.DATA, dataListener)
+
+        const decoder = new TinyH264Decoder({ canvas: canvasRef.current as HTMLCanvasElement });
+        void stream.pipeTo(decoder.writable)
+        sendMessage(WSMessageType.STREAM_METADATA_ACK)
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    if (canvasRef.current) {
+      start();
+    }
+
+    return () => {
+      unsubscribe(WSMessageType.CONFIGURATION, configListener)
+      unsubscribe(WSMessageType.DATA, dataListener)
+    }
+  }, [canvasRef]);
+
+  return (
+    <div className={streamingPhase !== StreamingPhase.RENDER ? 'hidden' : 'block'}>
+      <canvas ref={canvasRef} />
+    </div>
+  )
+}
+
+const WebCodecsRenderer: React.FC = () => {
+  const { sendMessage, subscribe, unsubscribe, streamingPhase } = useWebSocket();
+  const containerRef = useRef<HTMLDivElement>(null);
+  const controllerRef = useRef<ReadableStreamDefaultController<ScrcpyMediaStreamPacket> | null>(null)
+
+
+  const createVideoFrameRenderer = (): {
+    renderer: VideoFrameRenderer;
+    element: HTMLVideoElement | HTMLCanvasElement;
+  } => {
+    // Uncomment following lines to enable InsertableStreamVideoFrameRenderer, see quirks above
+    // if (InsertableStreamVideoFrameRenderer.isSupported) {
+    //   const renderer = new InsertableStreamVideoFrameRenderer();
+    //   return { renderer, element: renderer.element };
+    // }
+  
+    if (WebGLVideoFrameRenderer.isSupported) {
+      const renderer = new WebGLVideoFrameRenderer();
+      return { renderer, element: renderer.canvas as HTMLCanvasElement };
+    }
+  
+    const renderer = new BitmapVideoFrameRenderer();
+    return { renderer, element: renderer.canvas as HTMLCanvasElement };
+  };  
+
+  useEffect(() => {
+    const configListener: WSCallback = async (_metadata, binaryData) => {
+      if (!binaryData) return;
+      controllerRef.current?.enqueue({ type: "configuration", data: binaryData });
+      sendMessage(WSMessageType.CONFIGURATION_ACK)
+    }
+
+    const dataListener: WSCallback = async (m, binaryData) => {
+      const metadata = m as DataMetadata;
+      if (!binaryData) return;
+      controllerRef.current?.enqueue({
+        type: "data",
+        keyframe: metadata.keyframe,
+        pts: metadata.pts ? BigInt(metadata.pts) : undefined,
+        data: binaryData,
+      });
+    }
+
+    const start = () => {
+      try {
+        const stream = new ReadableStream<ScrcpyMediaStreamPacket>({
+          start(controller) {
+            controllerRef.current = controller;
+          }
+        });
+
+        subscribe(WSMessageType.CONFIGURATION, configListener)
+        subscribe(WSMessageType.DATA, dataListener)
+
+        const {renderer, element} = createVideoFrameRenderer();
+        (containerRef.current as HTMLDivElement).appendChild(element);
+        const decoder = new WebCodecsVideoDecoder({
+          codec: ScrcpyVideoCodecId.H264,
+          renderer: renderer as VideoFrameRenderer,
+        })
+        void stream.pipeTo(decoder.writable)
+        sendMessage(WSMessageType.STREAM_METADATA_ACK)
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    if (containerRef.current) {
+      start()
+    }
+
+    return () => {
+      unsubscribe(WSMessageType.CONFIGURATION, configListener)
+      unsubscribe(WSMessageType.DATA, dataListener)
+    }
+  }, [containerRef])
+
+  return <div className={streamingPhase !== StreamingPhase.RENDER ? 'hidden' : 'block'} ref={containerRef}/>;
+}
+
+const VideoRenderer: React.FC = () => {
+  const [rendererType, setRendererType] = useState<Renderer | null>();
+  const { subscribe, unsubscribe, streamingPhase } = useWebSocket();
+
+  useEffect(() => {
+    const metadataListener: WSCallback = (m) => {
+      const metadata = m as EnhancedStreamMetadata;
+      setRendererType(metadata.hardwareType === "hardware" ? Renderer.WebCodecs : Renderer.TinyH264)
+    }
+    subscribe(WSMessageType.STREAM_METADATA, metadataListener)
+
+    return () => {
+      unsubscribe(WSMessageType.STREAM_METADATA, metadataListener)
+    }
+  }, [subscribe, unsubscribe])
+
+  return (
+    <div className={`w-1/4 rounded-2xl border-8 border-black shadow-2xl flex items-center justify-center relative overflow-hidden ${streamingPhase !== StreamingPhase.RENDER ? 'aspect-[9/16] bg-gray-950' : 'bg-transparent '}`}>
+    {/* Speaker + Camera Dot */}
+    <div className="absolute top-2 w-full flex justify-center items-center z-10">
+      <div className="w-20 h-2 bg-black rounded-full"></div>
+      <div className="w-2 h-2 bg-black rounded-full ml-2"></div>
+    </div>
+    {streamingPhase !== StreamingPhase.RENDER && (
+        <div className="w-10/12">
+          <Lottie animationData={bootData} loop={true} />
+        </div>
+      )}
+    {rendererType === Renderer.TinyH264 && (
+      <TinyH264Renderer />
+    )}
+    {rendererType === Renderer.WebCodecs && (
+      <WebCodecsRenderer />
+    )}
+  </div>
+  )
+};
+
+
+import Logo from '@client/assets/logo.png'
+const Header = () => {
+  return (
+    <header className="w-full bg-gray-950 h-16">
+      <img src={Logo} className="h-[70px]" />
+    </header>
+  )
+}
+
 export const App = () => {
-    return <>DroidGround!</>
+    return (
+      <WebSocketProvider>
+        <div className="min-h-screen">
+          <Header />
+          <div className="container m-auto">
+            <VideoRenderer />
+          </div>
+        </div>
+      </WebSocketProvider>
+    )
 }
---
+++ src/client/context/WebSocket.tsx
@@ -0,0 +1,128 @@
+import {
+  createContext,
+  useEffect,
+  useRef,
+  useContext,
+  ReactNode,
+  useCallback,
+  useState,
+} from "react"
+import { StreamingPhase, WSCallback, WSMessageType } from "@shared/types"
+
+type WebSocketContextType = {
+  sendMessage: (message: string) => void
+  subscribe: (topic: WSMessageType, callback: WSCallback) => void
+  unsubscribe: (topic: WSMessageType, callback: WSCallback) => void
+  streamingPhase: StreamingPhase
+}
+
+const WebSocketContext = createContext<WebSocketContextType | undefined>(
+  undefined,
+)
+
+export const WebSocketProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [streamingPhase, setStreamingPhase] = useState<StreamingPhase>(StreamingPhase.INIT);
+  const socketRef = useRef<WebSocket | null>(null);
+  const listeners = useRef<Map<WSMessageType, Set<WSCallback>>>(new Map())
+
+  useEffect(() => {
+    const connect = async () => {
+      let webSocketURL = `ws://localhost:4242`
+      const socket = new WebSocket(webSocketURL)
+      socket.binaryType = 'arraybuffer';
+      socketRef.current = socket;
+
+      socket.onopen = () => console.log("WebSocket Connected")
+      socket.onmessage = event => {
+        try {
+            if (!(event.data instanceof ArrayBuffer)) {
+                console.warn('Received non-binary data:', event.data);
+                return;
+            }
+
+          const view = new DataView(event.data);
+          const metaLen = view.getUint32(0);
+          const full = new Uint8Array(event.data);
+          const metaBuf = full.slice(4, 4 + metaLen);
+          const binaryBuf = full.slice(4 + metaLen);
+        
+          const {type, ...metadata} = JSON.parse(new TextDecoder().decode(metaBuf));
+
+          switch (type) {
+            case WSMessageType.CONFIGURATION:
+              if (streamingPhase !== StreamingPhase.METADATA) {
+                setStreamingPhase(StreamingPhase.METADATA)
+              }
+              break
+            case WSMessageType.DATA:
+              if (streamingPhase !== StreamingPhase.RENDER) {
+                setStreamingPhase(StreamingPhase.RENDER)
+              }
+              break
+          }
+
+          // Notify specific topic listeners
+          listeners.current
+            .get(type)
+            ?.forEach(callback => callback(metadata, binaryBuf))
+
+
+        } catch (error) {
+            console.error(`WebSocket message parsing error: ${error}`)
+        }
+      }
+
+      socket.onerror = error => console.error(`WebSocket error: ${error}`)
+      socket.onclose = () => console.error("WebSocket Disconnected")
+    }
+
+    connect()
+  }, [])
+
+  const sendMessage = (message: string) => {
+    if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+      socketRef.current.send(message);
+    } else {
+      console.warn('WebSocket is not open. Message not sent:', message);
+    }
+  };
+
+  const subscribe = useCallback((topic: WSMessageType, callback: WSCallback) => {
+    if (!listeners.current.has(topic)) {
+      listeners.current.set(topic, new Set())
+    }
+    listeners.current.get(topic)?.add(callback)
+  }, [])
+
+  const unsubscribe = useCallback((topic: WSMessageType, callback: WSCallback) => {
+    listeners.current.get(topic)?.delete(callback)
+    if (listeners.current.get(topic)?.size === 0) {
+      listeners.current.delete(topic)
+    }
+  }, [])
+
+
+
+  return (
+    <WebSocketContext.Provider
+      value={{
+        sendMessage,
+        subscribe,
+        unsubscribe,
+        streamingPhase,
+      }}
+    >
+      {children}
+    </WebSocketContext.Provider>
+  )
+}
+
+export const useWebSocket = () => {
+  const context = useContext(WebSocketContext)
+  if (!context) {
+    throw new Error("useWebSocket must be used within a WebSocketProvider")
+  }
+  return context
+}
