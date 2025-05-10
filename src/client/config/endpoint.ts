e71e6f6a921beba79485286b865ff84d448d22ca
thelicato <thelicato@users.noreply.github.com>
2025-05-10 15:12:08 +0200

    feat: add client config

---
+++ src/client/config/endpoint.ts
@@ -0,0 +1,9 @@
+/*
+Configuration file containing constants
+Convention: constants are declared in UPPERCASE
+Usage: import { CONSTANT_NAME } from '@/config'
+*/
+
+export const BACKEND: { [properties: string]: string } = {
+    BASE_URL: '/api/v1',
+};
