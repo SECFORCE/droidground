22092b05a1dd32d1782407a4a9093c5e23ce02ad
thelicato <thelicato@users.noreply.github.com>
2025-05-21 15:33:32 +0200

    feat: add clipboard script

---
+++ library/clipboard.js
@@ -0,0 +1,30 @@
+/**
+ * Dump the clipboard content.
+ *
+ * @returns {void}
+ */
+rpc.exports = {
+  run: function () {
+    Java.perform(function () {
+      Java.choose("android.content.ClipboardManager", {
+        onMatch: function (instance) {
+          if (instance.toString().indexOf("android.content.ClipboardManager") >= 0) {
+            var ActivityThread = Java.use("android.app.ActivityThread");
+            var context = ActivityThread.currentApplication().getApplicationContext();
+            var ClipboardManager = Java.use("android.content.ClipboardManager");
+            var clipboardHandle = context.getSystemService("clipboard");
+            var cp = Java.cast(clipboardHandle, ClipboardManager);
+            var primaryClip = cp.getPrimaryClip();
+            if (primaryClip != null) {
+              send(primaryClip.toString());
+            }
+          }
+        },
+        onComplete: function () {},
+      });
+    });
+  },
+  schema: function () {
+    return null;
+  },
+};
