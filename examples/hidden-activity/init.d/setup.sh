d9fe370d0fa1eddc96c61f0e73ec66aae99bf2d1
thelicato <thelicato@users.noreply.github.com>
2025-07-06 18:58:48 +0200

    chore: update release version

--- examples/hidden-activity/init.d/setup.sh
+++ examples/hidden-activity/init.d/setup.sh
@@ -1,7 +1,7 @@
 
 #!/usr/bin/env bash
 
-wget https://github.com/SECFORCE/droidground-samples/releases/download/v0.1.0/hidden-activity-flag.apk
+wget https://github.com/SECFORCE/droidground-samples/releases/download/v0.1.2/hidden-activity-flag.apk
 adb shell pm uninstall com.droidground.hiddenactivity
 echo "Sleep for 2 seconds before installing app"
 sleep 2
