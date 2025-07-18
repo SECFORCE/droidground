252540c88c3c1b513507667d14f1b38674c3b91c
thelicato <thelicato@users.noreply.github.com>
2025-07-18 14:23:43 +0200

    chore: add spawner examples

--- examples/samples/hidden-activity/init.d/reset.sh
+++ examples/samples/hidden-activity/init.d/reset.sh
@@ -1,10 +1,10 @@
 
 #!/usr/bin/env bash
 
-wget https://github.com/SECFORCE/droidground-samples/releases/download/v0.1.2/hidden-activity-flag.apk
 adb shell pm uninstall com.droidground.hiddenactivity
 echo "Sleep for 2 seconds before installing app"
 sleep 2
 echo "Installing app..."
+# No need to download again the apk
 adb install ./hidden-activity-flag.apk
 echo "Install command executed"
