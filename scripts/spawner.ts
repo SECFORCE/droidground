778c7e3db5dc3ff662eeaa98ed416128ac01105f
thelicato <thelicato@users.noreply.github.com>
2025-07-18 16:31:31 +0200

    feat: setup spawner as an example

--- scripts/spawner.ts
+++
@@ -1,8 +0,0 @@
-import { $, fs } from "zx";
-
-$.verbose = true;
-
-await $`npm run build:setup`;
-await $`npm run build:client`;
-await $`npm run build:spawner`;
-await $`npm run copy-files`;
--- src/spawner/api/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import routes from "@spawner/api/routes";
-
-export default (): express.Router => {
-  const app: express.Router = express.Router();
-  routes(app);
-  return app;
-};
